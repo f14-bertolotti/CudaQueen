ac33bee620f8798b04e52c14a51cca3a1a5dc01e
f14-bertolotti <francesco.bertolotti@studenti.unipr.it>
2017-07-21 08:19:10 +0200

    adding memory managmemnt structure (has functions multi thread and multi block)

---
+++ MemoryManagement/MemoryManagementTest.cu
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "MemoryManagement.cu"
+
+///////////////////////////////////////////////////////////////////
+
+__device__ DeviceMemoryManagement deviceMemoryManagement;
+
+__global__ void init(int*,int,int,int);
+__global__ void test();
+
+///////////////////////////////////////////////////////////////////
+
+int main(){
+
+	HostMemoryManagement hostMemoryManagement(5,5,10);	
+	init<<<1,1>>>(hostMemoryManagement.dMem, hostMemoryManagement.rowSize,
+				  hostMemoryManagement.colSize, hostMemoryManagement.matSize);
+
+	test<<<1,1>>>();
+
+	cudaDeviceSynchronize();
+
+	return 0;
+}
+
+
+///////////////////////////////////////////////////////////////////
+
+__global__ void init(int* dMem, int rowSize, int colSize, int matSize){
+	deviceMemoryManagement.init(dMem,rowSize,colSize,matSize);
+}
+
+///////////////////////////////////////////////////////////////////
+
+__global__ void test(){
+	deviceMemoryManagement.setFromToMulti(0,0,0,4,4,9,0);
+	deviceMemoryManagement.setFromToMulti(0,0,0,0,4,0,2);
+	deviceMemoryManagement.setMatrix(2,3);
+	deviceMemoryManagement.setRow(2,2,4);
+	deviceMemoryManagement.setSingle(2,3,0,5);
+	deviceMemoryManagement.copyFromToMulti(2,0,3);
+	deviceMemoryManagement.copyFromToMulti(0,0,4);
+	deviceMemoryManagement.setFromToMulti(0,0,0,1,4,9,1);
+	deviceMemoryManagement.copyFromToMulti(2,3,2);
+
+	deviceMemoryManagement.print();
+
+}
+
+///////////////////////////////////////////////////////////////////
--- makefile
+++ makefile
@@ -7,10 +7,25 @@ LIB			= -L/usr/lib/x86_64-linux-gnu/
 make0:
 	$(NCC) ./CUDAqueen.cu -o ./bin/RCUDA $(NCCFLAGS) $(LIB)
 
-run-memcheck:
+make1:
+	$(NCC) ./MemoryManagement/MemoryManagementTest.cu -o ./bin/RMEMTEST $(NCCFLAGS) $(LIB)
+
+run0-memcheck:
 	make make0 && cuda-memcheck ./bin/RCUDA
 
-run: 
+run1-memcheck:
+	make make1 && cuda-memcheck ./bin/RMEMTEST
+
+run0: 
 	make make0 && ./bin/RCUDA
+
+run1: 
+	make make0 && ./bin/RMEMTEST
+
+clean0:
+	rm ./bin/RCUDA
+
+clean1:
+	rm ./bin/RMEMTEST
 
 all: make0
